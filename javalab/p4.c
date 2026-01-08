3dec8e8c85d01796dd80a386bdb1baf63efc6265
varshiniic <165465547+varshiniic@users.noreply.github.com>
2026-01-08 23:46:29 +0530

    Update p4.c

--- javalab/p4.c
+++ javalab/p4.c
@@ -1,1 +1,19 @@
+class Outer{
+	void display() {
+		System.out.println("This is the outer class display method");
+	}
+	class Inner{
+		void display() {
+			System.out.println("This is the inner class display method");
+		}
+	}
+}
+public class OuterInnerTest {
+	public static void main(String[] args) {
+		Outer outer=new Outer();
+		outer.display();
+		Outer.Inner inner = outer.new Inner();
+		inner.display();
+	}
 
+}
