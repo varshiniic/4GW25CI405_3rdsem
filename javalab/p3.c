70c8c413cfdd5f8321306299392c3526bb56eb06
varshiniic <165465547+varshiniic@users.noreply.github.com>
2026-01-08 23:45:35 +0530

    Update p3.c

--- javalab/p3.c
+++ javalab/p3.c
@@ -1,1 +1,24 @@
-
+class DivisionByZeroException extends Exception{
+	DivisionByZeroException(String message){
+		super(message);
+	}
+}
+public class DivisionTest {
+	public static void main(String[] args) {
+		int num=10;
+		int den=0;
+		try {
+			if(den==0) {
+				throw new DivisionByZeroException("Division by zero is not allowed!");
+			}
+			int result=num/den;
+			System.out.println("Result: "+result);
+		}
+		catch(DivisionByZeroException e) {
+			System.out.println("Exception caught: "+e.getMessage());
+		}
+		finally {
+			System.out.println("Finally bloch executed.");
+		}
+	}
+}
