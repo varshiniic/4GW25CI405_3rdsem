e6f3336d5cbe2de903e3b49f92cfe4276d43d8f6
varshiniic <165465547+varshiniic@users.noreply.github.com>
2026-01-08 23:44:51 +0530

    Update p2.c

--- javalab/p2.c
+++ javalab/p2.c
@@ -1,1 +1,62 @@
+import java.util.Scanner;
+//Employee class
+class Employee{
+	private int id;
+	private String name;
+	private double salary;
+	//Constructor to initialize employee details
+	public Employee(int id, String name, double salary) {
+		this.id =id;
+		this.name=name;
+		this.salary=salary;
+	}
+	//Method to display employee details
+	public void display() {
+		System.out.println("-----------------------------");
+		System.out.println("Employee ID: "+id);
+		System.out.println("Employee Name: "+name);
+		System.out.println("Employee Salary: $"+salary);
+		System.out.println("-----------------------------");
+	}
+	//Method to raise salary by a given percentage
+	public void raiseSalary(double percent) {
+		if(percent>0) {
+			double increment=salary *percent/100;
+			salary+=increment;
+			System.out.println("Salary increased by"+percent+"%($"+increment+")");
+		}
+		else {
+			System.out.println("Invalid percentage.Salary not changed");
+		}
+	}
+}
+//Main class
+public class EmployeeDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		//Input employee details
+		System.out.println("Enter Employee ID: ");
+		int id=sc.nextInt();
+		sc.nextLine();//Clear input buffer
+		System.out.println("Enter Employee Name: ");
+		String name =sc.nextLine();
+		System.out.println("Enter Employee Salary: ");
+		double salary =sc.nextDouble();
+		//create Employee object
+		Employee emp = new Employee(id,name,salary);
+		//Display initial details
+		System.out.println("\n---Employee Details Before Salary Raise---");
+		emp.display();
+		//Input raise percentage
+		System.out.println("\nEnter percentage to raise salary: ");
+		double percent = sc.nextDouble();
+		//apply raise
+		emp.raiseSalary(percent);
+		//Display updated details
+		System.out.println("\n-----Employee Details After Salary Raise----");
+		emp.display();
+		sc.close();
+	}
 
+}
